4d8125b6892088c115b420e44369bdab55271a2d
LukinhasAd09 <dornelaslucas190@gmail.com>
2025-04-25 09:30:10 -0300

    desafio dio

---
+++ index.js
@@ -0,0 +1,19 @@
+// entradada de dados
+System.out.print("Digite o nome do heroi: ");
+stringnome =
+System.out.print ("digite o xp: ");
+stringxp = 
+// variavel para armazenar o nivel;
+Stringnivel 
+// estrutura de decisao;
+if (xp<1000){nivel = ferro
+}else if (xp <= 1001 && 2000){nivel = bronze;
+}else if (xp <= 2001 && 5000){nivel = prata;
+}else if (xp <= 5001 && 7000){nivel = ouro;
+}else if (xp <= 7001 && 8000){nivel = platina;
+}else if (xp <= 8001 && 9000){nivel = ascendente;
+}else if (xp <= 9001 && 10000){nivel = imortal;
+}else if (xp >= 10001){ nivel = radiante;
+}
+// saida final ;
+System.out.pritnln ("**O herói de nome **" + nome + "** está no nível de **" + nivel );
